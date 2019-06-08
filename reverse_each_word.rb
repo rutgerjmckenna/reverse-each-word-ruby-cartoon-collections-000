87479395127d6b44f23c41ad468ee48f4ef2f42a
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-08 17:58:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 sentence1 = ["Hello", "there,", "how", "are", "you?"]
 
 def reverse_each_word(words)
-  words.each do |x|
+  words.collect do |x|
     puts x.reverse
   end
 end
 
 reverse_each_word(sentence1)
-
