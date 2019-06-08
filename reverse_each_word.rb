7c60fd734b998ea186ffcd5c8c4fda78a69f9bf9
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-06-08 18:09:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
-sentence1 = ["Hello", "there,", "how", "are", "you?"]
-
-def reverse_each_word(words)
-  words.each do |x|
-    puts x.reverse
-  end
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
 
-reverse_each_word(sentence1)
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
+
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
